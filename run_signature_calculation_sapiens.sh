897482cfaa4fbb0a13a406fc6d4c6ac2dd472363
FilyCode <phitro@bu.edu>
2025-11-21 20:45:11 -0500

    small adjustment

--- run_signature_calculation_sapiens.sh
+++ run_signature_calculation_sapiens.sh
@@ -7,7 +7,7 @@
 #$ -P agedisease
 
 # Hard time limit (hh:mm:ss). Adjust as needed.
-#$ -l h_rt=48:00:00
+#$ -l h_rt=12:00:00
 
 # Job name
 #$ -N RAgeSignatureCalculation
@@ -22,7 +22,7 @@
 #$ -M phitro@bu.edu
 
 # Request cores.
-#$ -pe omp 32
+#$ -pe omp 48
 
 # Request total memory for the job.
 #$ -l mem_total=400G
